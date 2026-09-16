654c887e8489b489b7b26d3c137a80c0233a15d3
Shashidhar799 <pathishashidharreddy@gmail.com>
2026-09-16 22:57:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search in Linked List/search-in-linked-list.java
@@ -0,0 +1,24 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public boolean searchKey(Node head, int key) {
+        // Code here
+        Node current=head;
+        while(current!=null){
+            if(current.data==key){
+                return true;
+            }
+            current=current.next;
+        }
+        return false;
+    }
+}
